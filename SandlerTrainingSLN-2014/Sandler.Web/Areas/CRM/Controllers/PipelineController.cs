17502f09d00a5a4fe4b3f0501ea0e8cb68a4aa81
Jignesh Shah <jigsh@hotmail.com>
2014-03-03 23:53:06 -0500

    OpportunityExcelDownload done

--- SandlerTrainingSLN-2014/Sandler.Web/Areas/CRM/Controllers/PipelineController.cs
+++ SandlerTrainingSLN-2014/Sandler.Web/Areas/CRM/Controllers/PipelineController.cs
@@ -4,9 +4,12 @@ using Sandler.DB.Data.Common.Interface;
 using Sandler.DB.Data.Repositories;
 using Sandler.DB.Models;
 using Sandler.Web.Controllers;
+using Sandler.Web.Library;
+using Sandler.Web.Models;
 using Sandler.Web.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -41,5 +44,32 @@ namespace Sandler.Web.Areas.CRM.Controllers
             return PartialView("Manage", PipelineViewModel);
         }
 
+        public ActionResult ExportCompany()
+        {
+
+            string sToday = String.Format("{0:yyyyMMMdd__hh_mm_ss tt}", DateTime.Now);
+            string moduleName = "Pipeline";
+
+            return new ExcelResult
+            {
+                fileName = moduleName + "_" + System.Guid.NewGuid() + "_" + sToday + ".xlsx",
+                filePath = "~/Downloads/",
+                sheetName = moduleName,
+                clientsidefileName = moduleName + "_" + sToday + ".xlsx",
+                sqlStatement = GetSQLStatement(0),
+                connectionSring = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString
+            };
+
+        }
+
+        public string GetSQLStatement(int companyId)
+        {
+            string whereClause = "";
+            if (companyId > 0)
+                whereClause = whereClause + ", @companyId=" + companyId;
+
+            return string.Format("exec [sp_OpportunityView] @userId='{0}', @orderBy='{1}', @pageSize={2}, @pageNo={3}{4}"
+                , BaseVM.CurrentUser.UserId.ToString(), "NAME ASC", 0, 0, whereClause);
+        }
     }
 }
