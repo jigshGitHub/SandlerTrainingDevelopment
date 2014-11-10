5487eb3b3230891a254174b88243019578d41872
jigshGitHub <jigsh@hotmail.com>
2014-11-10 12:53:35 -0500

    SPM2-Changes-11102014-1
    LDAP implementation changes in AccountController - 1 hr

--- SandlerTrainingSLN-2014/Sandler.Web/Controllers/AccountController.cs
+++ SandlerTrainingSLN-2014/Sandler.Web/Controllers/AccountController.cs
@@ -81,12 +81,11 @@ namespace Sandler.Web.Controllers
         {
              if (ModelState.IsValid)
             {
+                MembershipUser loggedInUser = Membership.GetUser(model.UserName);
+                    
                 if (Membership.ValidateUser(model.UserName, model.Password))
                 {
                     FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
-                    MembershipUser loggedInUser = Membership.GetUser(model.UserName);
-                    //if (loggedInUser.CreationDate == loggedInUser.LastPasswordChangedDate)
-                    //    return RedirectToAction("ChangePassword", "account", new { isIntialPasswordChange = true });
                     if (Url.IsLocalUrl(returnUrl))
                     {
                         return Redirect(returnUrl);
@@ -95,6 +94,10 @@ namespace Sandler.Web.Controllers
                     {
                         return RedirectToAction("Index", "Home");
                     }
+                }
+                else if (loggedInUser != null && LDAPValidation(System.Configuration.ConfigurationManager.AppSettings["ldapDomainAddress"], model.UserName, model.Password))
+                {
+                    //Process anything after LDAP validation
                 }
                 else
                 {
@@ -222,50 +225,7 @@ namespace Sandler.Web.Controllers
             return View();
         }
 
-        private IEnumerable<string> GetErrorsFromModelState()
-        {
-            return ModelState.SelectMany(x => x.Value.Errors.Select(error => error.ErrorMessage));
-        }
-
-        #region Status Codes
-        private static string ErrorCodeToString(MembershipCreateStatus createStatus)
-        {
-            // See http://go.microsoft.com/fwlink/?LinkID=177550 for
-            // a full list of status codes.
-            switch (createStatus)
-            {
-                case MembershipCreateStatus.DuplicateUserName:
-                    return "User name already exists. Please enter a different user name.";
-
-                case MembershipCreateStatus.DuplicateEmail:
-                    return "A user name for that e-mail address already exists. Please enter a different e-mail address.";
-
-                case MembershipCreateStatus.InvalidPassword:
-                    return "The password provided is invalid. Please enter a valid password value.";
-
-                case MembershipCreateStatus.InvalidEmail:
-                    return "The e-mail address provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.InvalidAnswer:
-                    return "The password retrieval answer provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.InvalidQuestion:
-                    return "The password retrieval question provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.InvalidUserName:
-                    return "The user name provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.ProviderError:
-                    return "The authentication provider returned an error. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
-
-                case MembershipCreateStatus.UserRejected:
-                    return "The user creation request has been canceled. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
-
-                default:
-                    return "An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
-            }
-        }
-        #endregion
+        
 
         public ActionResult ManageUsers()
         {
@@ -319,6 +279,71 @@ namespace Sandler.Web.Controllers
                 profile.RoleEntityValue = roleEntityValue;
         }
         
+#region Private Methods
+
+        private IEnumerable<string> GetErrorsFromModelState()
+        {
+            return ModelState.SelectMany(x => x.Value.Errors.Select(error => error.ErrorMessage));
+        }
+
+        #region Status Codes
+        private static string ErrorCodeToString(MembershipCreateStatus createStatus)
+        {
+            // See http://go.microsoft.com/fwlink/?LinkID=177550 for
+            // a full list of status codes.
+            switch (createStatus)
+            {
+                case MembershipCreateStatus.DuplicateUserName:
+                    return "User name already exists. Please enter a different user name.";
+
+                case MembershipCreateStatus.DuplicateEmail:
+                    return "A user name for that e-mail address already exists. Please enter a different e-mail address.";
+
+                case MembershipCreateStatus.InvalidPassword:
+                    return "The password provided is invalid. Please enter a valid password value.";
+
+                case MembershipCreateStatus.InvalidEmail:
+                    return "The e-mail address provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidAnswer:
+                    return "The password retrieval answer provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidQuestion:
+                    return "The password retrieval question provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.InvalidUserName:
+                    return "The user name provided is invalid. Please check the value and try again.";
+
+                case MembershipCreateStatus.ProviderError:
+                    return "The authentication provider returned an error. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+
+                case MembershipCreateStatus.UserRejected:
+                    return "The user creation request has been canceled. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+
+                default:
+                    return "An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
+            }
+        }
+        #endregion
+
+        private bool LDAPValidation(string ldapDomain, string userName, string password)
+        {
+            System.DirectoryServices.DirectoryEntry de = new System.DirectoryServices.DirectoryEntry(@"LDAP://" + ldapDomain, userName, password);
+
+            try
+            {
+                object o = de.NativeObject;
+                //This means LDAP has found the entry with username and Password
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //Either User does not exists or password is Wrong
+                throw new Exception("LDAP validation failed:" + ex.Message);
+            }
+        }
+
+#endregion
     }
     #region Original Code Genereted
     //[Authorize]
